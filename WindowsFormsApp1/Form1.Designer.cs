cea47b4eb9a0801f9dbe5267345dbefe66c08edf
Baby Pooh <babybearbear1280@gmail.com>
2022-12-09 07:11:13 -0500

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button_extract = new System.Windows.Forms.Button();
+            this.checkboxlist_regExpList = new System.Windows.Forms.CheckedListBox();
+            this.button_go = new System.Windows.Forms.Button();
+            this.button_regexp = new System.Windows.Forms.Button();
+            this.textbox_url = new System.Windows.Forms.TextBox();
+            this.label_url = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataSet1 = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1151, 450);
+            this.splitContainer1.SplitterDistance = 203;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button_extract);
+            this.splitContainer2.Panel1.Controls.Add(this.checkboxlist_regExpList);
+            this.splitContainer2.Panel1.Controls.Add(this.button_go);
+            this.splitContainer2.Panel1.Controls.Add(this.button_regexp);
+            this.splitContainer2.Panel1.Controls.Add(this.textbox_url);
+            this.splitContainer2.Panel1.Controls.Add(this.label_url);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.splitContainer2.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Size = new System.Drawing.Size(1151, 203);
+            this.splitContainer2.SplitterDistance = 461;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // button_extract
+            // 
+            this.button_extract.Location = new System.Drawing.Point(278, 72);
+            this.button_extract.Name = "button_extract";
+            this.button_extract.Size = new System.Drawing.Size(92, 23);
+            this.button_extract.TabIndex = 5;
+            this.button_extract.Text = "Extract";
+            this.button_extract.UseVisualStyleBackColor = true;
+            this.button_extract.Click += new System.EventHandler(this.button_extract_Click);
+            // 
+            // checkboxlist_regExpList
+            // 
+            this.checkboxlist_regExpList.FormattingEnabled = true;
+            this.checkboxlist_regExpList.Location = new System.Drawing.Point(51, 101);
+            this.checkboxlist_regExpList.Name = "checkboxlist_regExpList";
+            this.checkboxlist_regExpList.Size = new System.Drawing.Size(319, 79);
+            this.checkboxlist_regExpList.TabIndex = 3;
+            // 
+            // button_go
+            // 
+            this.button_go.Location = new System.Drawing.Point(390, 22);
+            this.button_go.Name = "button_go";
+            this.button_go.Size = new System.Drawing.Size(48, 23);
+            this.button_go.TabIndex = 2;
+            this.button_go.Text = "Go";
+            this.button_go.UseVisualStyleBackColor = true;
+            this.button_go.Click += new System.EventHandler(this.button_go_Click);
+            // 
+            // button_regexp
+            // 
+            this.button_regexp.Location = new System.Drawing.Point(51, 72);
+            this.button_regexp.Name = "button_regexp";
+            this.button_regexp.Size = new System.Drawing.Size(92, 23);
+            this.button_regexp.TabIndex = 4;
+            this.button_regexp.Text = "Import RegExp";
+            this.button_regexp.UseVisualStyleBackColor = true;
+            this.button_regexp.Click += new System.EventHandler(this.button_regexp_Click);
+            // 
+            // textbox_url
+            // 
+            this.textbox_url.Location = new System.Drawing.Point(51, 24);
+            this.textbox_url.Name = "textbox_url";
+            this.textbox_url.Size = new System.Drawing.Size(319, 20);
+            this.textbox_url.TabIndex = 1;
+            // 
+            // label_url
+            // 
+            this.label_url.AutoSize = true;
+            this.label_url.Location = new System.Drawing.Point(16, 27);
+            this.label_url.Name = "label_url";
+            this.label_url.Size = new System.Drawing.Size(29, 13);
+            this.label_url.TabIndex = 0;
+            this.label_url.Text = "URL";
+            this.label_url.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(686, 146);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Extract Result";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button_go;
+        private System.Windows.Forms.TextBox textbox_url;
+        private System.Windows.Forms.Label label_url;
+        private System.Windows.Forms.Button button_regexp;
+        private System.Windows.Forms.CheckedListBox checkboxlist_regExpList;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_extract;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet dataSet1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+/**
+ * Cefsharp Example
+ * 
+ * BabyBear Project
+ * 12/09/2022
+ */
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        // Initial Url
+        private string initUrl = "https://www.imoradar24.ro/apartamente-de-vanzare/iasi-iasi";
+        // Map for store reg exp
+        private Dictionary<string, string> regExpMap = new Dictionary<string, string>();
+        // for store html of current url
+        private string html = string.Empty;
+        // Embeded browser
+        public ChromiumWebBrowser browser;
+
+        /**
+         * Initialize Browser
+         */
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser(initUrl);
+            splitContainer1.Panel2.Controls.Add(browser);
+            splitContainer1.Dock = DockStyle.Fill;
+
+            browser.FrameLoadEnd += WebBrowserFrameLoadEnded;
+
+
+            textbox_url.Text = initUrl;
+        }
+
+        /**
+         * Add EventListener for get html when end load
+         */
+        private void WebBrowserFrameLoadEnded(object sender, FrameLoadEndEventArgs e)
+        {
+            if (e.Frame.IsMain)
+            {
+                browser.ViewSource();
+                browser.GetSourceAsync().ContinueWith(taskHtml =>
+                {
+                    html = taskHtml.Result;
+                });
+            }
+        }
+
+        /**
+         * Load Browser
+         */
+        public void LoadBrowser(string url)
+        {
+            browser.Load(url);
+        }
+
+        /**
+         * Initialize Windows Form
+         */
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitBrowser();
+        }
+
+        /**
+         * Go button / Click Event
+         */
+        private void button_go_Click(object sender, EventArgs e)
+        {
+            string url = textbox_url.Text;
+
+            if (string.IsNullOrEmpty(url))
+            {
+                return;
+            }
+
+            LoadBrowser(url);
+        }
+
+        /**
+         * Import RegExp button / Click Event
+         */
+        private void button_regexp_Click(object sender, EventArgs e)
+        {
+            checkboxlist_regExpList.Items.Clear();
+
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                //Read the contents of the file into a stream
+                var fileContent = string.Empty;
+                var fileStream = openFileDialog1.OpenFile();
+                
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    // Import RegExp list
+                    while (!reader.EndOfStream)
+                    {
+                        string temp = reader.ReadLine();
+
+                        if (temp.IndexOf("-->") > 0)
+                        {
+                            checkboxlist_regExpList.Items.Add(temp);
+                        }
+                    }
+                }
+
+
+            }
+            else
+            {
+                //Operation aborted by the user
+            }
+        }
+
+        /**
+         * Extract RegExp button / Click Event
+         */
+        private void button_extract_Click(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+
+            table.Columns.Add(new DataColumn("#"));
+
+            HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+
+            htmlDoc.OptionFixNestedTags = true;
+            htmlDoc.OptionAutoCloseOnEnd = true;
+            htmlDoc.OptionCheckSyntax = false;
+
+            HtmlAgilityPack.HtmlNode.ElementsFlags.Remove("option");
+
+            htmlDoc.LoadHtml(html);
+
+            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
+            {
+                // Handle any parse errors as required
+                return;
+            }
+            else
+            {
+
+                if (htmlDoc.DocumentNode != null)
+                {
+
+                    foreach (string item in checkboxlist_regExpList.CheckedItems)
+                    {
+                        if (item.IndexOf("-->") > 0)
+                        {
+                            //dataGridView1.Columns[index].Name = item.Substring(0, item.IndexOf("-->"));
+
+                            table.Columns.Add(new DataColumn(item.Substring(0, item.IndexOf("-->"))));
+
+                            // Get reg exp data from checkbox list
+                            String selector = item.Substring(item.IndexOf("-->") + 3).Trim();
+
+                            HtmlAgilityPack.HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes(selector);
+
+                            int count = 1;
+                            if (nodes.Count > 0)
+                            {
+                                // Do something with bodyNode
+                                foreach(var node in nodes)
+                                {
+                                    string link = node.GetAttributeValue("href", "default");
+
+                                    if (string.IsNullOrEmpty(link) || link.Equals("#"))
+                                    {
+                                        continue;
+                                    }
+
+                                    table.Rows.Add(new object[] { count ++, link });
+                                }
+                            }
+
+                            //// convert string to regexp
+                            //Regex rg = new Regex(selector, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                            //MatchCollection matchedAuthors = rg.Matches(html);
+
+
+                            //for (int count = 0; count < matchedAuthors.Count; count++)
+                            //{
+                            //    //Console.WriteLine(matchedAuthors[count].Value);
+
+                            //    if (string.IsNullOrEmpty(matchedAuthors[count].Value))
+                            //    {
+                            //        continue;
+                            //    }
+
+                            //    table.Rows.Add(new object[] { count + 1, matchedAuthors[count].Value });
+                            //}
+                        }
+                    }
+
+                    
+                }
+            }
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+        }
+    }
+}
